9639464cc65f0c267347018d57f31a1328063d0b
Ashraful Haque <vselfnet@gmail.com>
2023-05-17 20:06:51 +0200

    Navbar & Footer Setup

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -6,7 +6,9 @@ const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='max-w-7xl mx-auto'>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,48 @@
+import { Link } from "react-router-dom";
+import logo from '../../../assets/car-side-solid.svg'
 
 const Navbar = () => {
+    const navItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to=''>Toys </Link></li>
+        <li><Link to=''>My Toys</Link></li>
+        <li><Link to=''>Add Toy</Link></li>
+        <li> <Link to=''>Login</Link></li>
+    </>
+
     return (
-        <div>
-            <h3>nav</h3>
+        <div className="navbar bg-base-300">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItems}
+                    </ul>
+                </div>
+                <Link to='/'>
+                    <div className="flex gap-5">
+                        <p className="text-3xl">Toyazone</p>
+                        <img className="w-10 rounded-full" src={logo} alt="logo" />
+                    </div>
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navItems}
+                </ul>
+                {/* {
+                    user && user.email
+                } */}
+            </div>
+            <div className="navbar-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                </label>
+            </div>
         </div>
     );
 };
